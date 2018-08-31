546906d5c57f243dd371aca5935398682679a80e
Jakob van Santen <jvansanten@gmail.com>
2018-08-31 21:24:51 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='ampel-contrib-weizmann',
-      version='0.3.0',
+      version='0.4.0',
       packages=['ampel.contrib.weizmann',
                 'ampel.contrib.weizmann.t0'],
       package_data = {'': ['*.json']},
