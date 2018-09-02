fb63f0e15ed4bb6ff38f1bdbae4f1cb2819fd744
Jakob van Santen <jvansanten@gmail.com>
2018-09-02 20:26:03 +0200

    Actually register T3 jobs

--- setup.py
+++ setup.py
@@ -13,5 +13,11 @@ setup(name='ampel-contrib-weizmann',
           'ampel.pipeline.t0' : [
               'InfantFilter = ampel.contrib.weizmann.t0.InfantFilter:InfantFilter',
           ],
+          'ampel.pipeline.t3.jobs' : [
+              'weizmann = ampel.contrib.weizmann.channels:load_t3_jobs',
+          ],
+          'ampel.pipeline.t3.configs' : [
+              'weizmann = ampel.contrib.weizmann.channels:load_t3_run_configs',
+          ],
       }
 )
